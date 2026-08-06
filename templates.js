1b178f41b5a57021f0fb4ca24129e8e90f170c71
Sven Apel <svap1201@gmail.com>
2026-08-06 11:08:50 +0200

    Add main tab in diolog overlay

--- templates.js
+++ templates.js
@@ -29,28 +29,57 @@ function getDialogTemplate(pokemon) {
     `;
 }
 
+function renderAbilities(pokemon) {
+    return pokemon.abilities.map(entry => entry.ability.name).join(', ');
+}
+
 function getMainTab(pokemon) {
-    return "Main kommt später hier.";
+    const heightMeters = (pokemon.height / 10).toFixed(1);
+    const weightKg = (pokemon.weight / 10).toFixed(1);
+	return /*html*/ `
+        <ul>
+            <li class="info-row">
+                <span class="label">Height</span>
+                <span class="separator">:</span>
+                <span class="value">${heightMeters} m</span>
+            </li>
+            <li class="info-row">
+                <span class="label">Weight</span>
+                <span class="separator">:</span>
+                <span class="value">${weightKg} kg</span>
+            </li>
+            <li class="info-row">
+                <span class="label">Base experience</span>
+                <span class="separator">:</span>
+                <span class="value">${pokemon.base_experience}</span>
+            </li>
+            <li class="info-row">
+                <span class="label">Abilities</span>
+                <span class="separator">:</span>
+                <span class="value">${renderAbilities(pokemon)}</span>
+            </li>
+        </ul>
+    `;
 }
 
 function getStatsTab(pokemon) {
-    return /*html*/`
+	return /*html*/ `
         <ul class="stats">${renderStats(pokemon)}</ul>
-    `
+    `;
 }
 
 function getEvoTab(pokemon) {
-    return "EVO kommt später hier.";
+	return "EVO kommt später hier.";
 }
 
 function getTabContent(pokemon) {
-    if (activeTab === "main") {
-        return getMainTab(pokemon);
-    } else if (activeTab === "stats") {
-        return getStatsTab(pokemon);
-    } else if (activeTab === "evo") {
-        return getEvoTab(pokemon);
-    }
+	if (activeTab === "main") {
+		return getMainTab(pokemon);
+	} else if (activeTab === "stats") {
+		return getStatsTab(pokemon);
+	} else if (activeTab === "evo") {
+		return getEvoTab(pokemon);
+	}
 }
 
 function getNotFoundTemplate() {
